5893083f3edc2c49fbc8b44084b10f4e064a9980
Maryam <unfixablebug26@gmail.com>
2025-09-15 13:36:14 +0300

    Parser with summariser

--- data_extraction.py
+++ data_extraction.py
@@ -1,59 +1,97 @@
-from unstructured.partition.pdf import partition_pdf
-from unstructured.documents.elements import Image, Table, CompositeElement
-from IPython.display import display, HTML, Image
-import base64
-import os
+def process_text_and_tables(chunks):
+    from langchain_core.prompts import ChatPromptTemplate
+    from langchain_core.output_parsers import StrOutputParser
+    from langchain_groq import ChatGroq
 
-for number in range(3):
-    file_path = f"./inputs/report{number}.pdf"
-    os.makedirs(f"./outputs/report{number}", exist_ok=True)
-    # Reference: https://docs.unstructured.io/open-source/core-functionality/chunking
-    chunks = partition_pdf(
-        filename=file_path,
-        infer_table_structure=True,            # extract tables
-        strategy="hi_res",                     # mandatory to infer tables
-        # for image extractions
-        extract_image_block_types=["Image"],   # Add 'Table' to list to extract image of tables
-        extract_image_block_to_payload=True,
-        # chunking_strategy=None,
-    # )
-    # text_chunks = partition_pdf(
-        # filename=file_path,
-        # infer_table_structure=True,     
-        # strategy="hi_res",                
+    from dotenv import load_dotenv
+    load_dotenv()
 
-        chunking_strategy="by_title", 
-        max_characters=10000, 
-        combine_text_under_n_chars=2000,
-        new_after_n_chars=6000,
-    )
-    def get_images_base64(chunks):
-        images_b64 = []
-        for chunk in chunks:
-            if "CompositeElement" in str(type(chunk)):
-                chunk_els = chunk.metadata.orig_elements
-                for el in chunk_els:
-                    if "Image" in str(type(el)):
-                        images_b64.append(el.metadata.image_base64)
-        return images_b64
-    
-    images = get_images_base64(chunks)
     texts = [chunk for chunk in chunks if chunk.category != "Table" and chunk.text and chunk.text.strip()]
     temp_tables = [chunk for chunk in chunks if chunk.category == "Table" ]
     tables = [table.metadata.text_as_html for table in temp_tables if hasattr(table.metadata, 'text_as_html') and table.metadata.text_as_html]
 
-    # exporting the image
-    for i, image_data in enumerate(images):
-        img_data = base64.b64decode(image_data)
-        with open(f"./outputs/report{number}/image_{i+1}.png", "wb") as f:
-            f.write(img_data)
+    prompt_text = """
+You are an assistant tasked with summarizing tables and text.
+Give a concise summary of the table or text.
 
-    # exporting the table as html
-    for i, table in enumerate(tables):
-        with open(f"./outputs/report{number}/table_{i+1}.html", "w", encoding="utf-8") as f:
-            f.write(table)
+Respond only with the summary, no additionnal comment.
+Do not start your message by saying "Here is a summary" or anything like that.
+Just give the summary as it is.
 
-    # exporting texts
-    with open(f"./outputs/report{number}/extracted_text.txt", "w", encoding="utf-8") as f:
-        for chunk in texts:
-            f.write(chunk.text)
+Table or text chunk: {element}
+
+"""
+    prompt = ChatPromptTemplate.from_template(prompt_text)
+
+    # Summary chain
+    model = ChatGroq(temperature=0.5, model="llama-3.1-8b-instant")
+    summarize_chain = {"element": lambda x: x} | prompt | model | StrOutputParser()
+    # Summarize text
+    text_summaries = summarize_chain.batch(texts, {"max_concurrency": 3})
+
+    # Summarize tables
+    tables_html = [table.metadata.text_as_html for table in tables]
+    table_summaries = summarize_chain.batch(tables_html, {"max_concurrency": 3})
+    return text_summaries, table_summaries
+
+def process_images(chunks):
+    import base64
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+    
+    images = []
+    for chunk in chunks:
+        if "CompositeElement" in str(type(chunk)):
+            chunk_els = chunk.metadata.orig_elements
+            for el in chunk_els:
+                if "Image" in str(type(el)):
+                    images.append(el.metadata.image_base64)
+
+    from langchain_openai import ChatOpenAI
+    from langchain_core.prompts import ChatPromptTemplate
+    from langchain_core.output_parsers import StrOutputParser
+
+    prompt_template = """Describe the image in detail. For context,
+                    the image is part of a research paper explaining the transformers
+                    architecture. Be specific about graphs, such as bar plots."""
+    messages = [
+        (
+            "user",
+            [
+                {"type": "text", "text": prompt_template},
+                {
+                    "type": "image_url",
+                    "image_url": {"url": "data:image/jpeg;base64,{image}"},
+                },
+            ],
+        )
+    ]
+    prompt = ChatPromptTemplate.from_messages(messages)
+    chain = prompt | ChatOpenAI(model="gpt-4o-mini") | StrOutputParser()
+    image_summaries = chain.batch(images)
+    return image_summaries
+
+
+# def create_chunks(report = "./inputs/report0.pdf"):
+report = "./inputs/report0.pdf"
+from unstructured.partition.pdf import partition_pdf
+from unstructured.documents.elements import Image, Table, CompositeElement
+# Reference: https://docs.unstructured.io/open-source/core-functionality/chunking
+chunks = partition_pdf(
+    filename=report,
+    infer_table_structure=True,            # extract tables
+    strategy="hi_res",                     # mandatory to infer tables
+    # for image extractions
+    extract_image_block_types=["Image"],   # Add 'Table' to list to extract image of tables
+    extract_image_block_to_payload=True,
+    chunking_strategy="by_title", 
+    max_characters=10000, 
+    combine_text_under_n_chars=2000,
+    new_after_n_chars=6000,
+)
+# images = process_images(chunks)
+texts, tables = process_text_and_tables(chunks)
+# print(images[0])
+print(texts[0])
+print(tables[0])
